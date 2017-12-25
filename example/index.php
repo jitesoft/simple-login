f7deab9c3ac934e091d684b8269df7e353e8eb20
Johannes <johannes@jitesoft.com>
2017-12-25 18:53:04 +0100

    Minor fix to example code.

--- example/index.php
+++ example/index.php
@@ -1,10 +1,10 @@
 <?php
 require_once '../vendor/autoload.php';
 use Jitesoft\Container\Container;
+use Jitesoft\SimpleLogin\AuthenticableRepositoryInterface;
 use Jitesoft\SimpleLogin\Authenticator;
-use Jitesoft\SimpleLogin\Contracts\AuthenticableInterface;
-use Jitesoft\SimpleLogin\Contracts\AuthenticableRepositoryInterface;
-use Jitesoft\SimpleLogin\Contracts\AuthenticatorInterface;
+use Jitesoft\SimpleLogin\AuthenticableInterface;
+use Jitesoft\SimpleLogin\AuthenticatorInterface;
 use Jitesoft\SimpleLogin\Crypto\CryptoInterface;
 use Psr\Container\ContainerInterface;
 
